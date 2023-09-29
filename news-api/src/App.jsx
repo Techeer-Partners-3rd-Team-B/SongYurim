065f00b62cab33f1b6f3b987da31868edf114143
youlimsongs <giuhki@naver.com>
2023-09-29 16:01:52 +0900

    category components 구현

--- news-api/src/App.jsx
+++ news-api/src/App.jsx
@@ -1,10 +1,17 @@
-import { React } from 'react';
+import { React, useCallback, useState } from 'react';
 import './App.css';
-import NewsList from './NewsList';
+import Categories from './components/Categories';
+import NewsList from './components/NewsList';
 
 function App(){
+  const [category, setCategory] = useState('all')
+  const onSelect = useCallback(Category => setCategory(Category), [])
   return(
-    <NewsList/>
+    <>
+    {/* props로 카테고리 state와 함수를 넘겨줌 */}
+      <Categories category={category} onSelect={onSelect}/>
+      <NewsList category={category}/>
+    </>
   );
 }
 
