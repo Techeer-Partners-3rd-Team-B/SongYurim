6d1b7dfa633a1b9c21e9e734c20941c894e8be5a
youlimsongs <giuhki@naver.com>
2023-10-09 19:39:00 +0900

    Loading-Screen 구현

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Login from "./components/login"
 import CreateAccount from "./components/create-account"
 import { createGlobalStyle } from "styled-components"
 import reset from "styled-reset"
+import { useEffect, useState } from "react"
+import LoadingScreen from "./components/loading-screen"
 
 const router = createBrowserRouter([
   {
@@ -48,13 +50,21 @@ const GlobalStyles = createGlobalStyle`
 
 
 function App() {
+  const [isLoading, setLoading] = useState(true);  
+  const init = async () => {
+    // wait for firebase  
+    setLoading(false);
+  };
+  useEffect(()=> {
+    init();
+  }, []);
 
   return (
     <>
       <GlobalStyles/>
-      <RouterProvider router={router}/>
+      {isLoading? <LoadingScreen/> : <RouterProvider router={router}/>}
     </>
-  )
+  );
 }
 
 export default App
