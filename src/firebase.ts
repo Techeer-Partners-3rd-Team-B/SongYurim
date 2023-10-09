ac850e50354e960163bf869c07a50c5e67d007f8
youlimsongs <giuhki@naver.com>
2023-10-09 19:57:43 +0900

    create firebase project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsCEwBvtTp-9lDw2kb9On1wsCwl8oUMyQ",
+  authDomain: "nwitter-reloaded-7f8e0.firebaseapp.com",
+  projectId: "nwitter-reloaded-7f8e0",
+  storageBucket: "nwitter-reloaded-7f8e0.appspot.com",
+  messagingSenderId: "308164398439",
+  appId: "1:308164398439:web:b1dd0de77e08d1a7b173c2"
+};
+
+const app = initializeApp(firebaseConfig);
